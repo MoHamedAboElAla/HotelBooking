fe7e5ded65295163673435a924cd9dfa696c252f
MohamedAboElAla <mohamed20300005@gmail.com>
2025-07-02 13:34:52 +0300

    Add DbContext Class and migrations

--- HotelBooking.Infrastructure/Data/AppDbContext.cs
+++ HotelBooking.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelBooking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace HotelBooking.Infrastructure.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+         public DbSet<Hotel> Hotels { get; set; }
+         public DbSet<Room> Rooms { get; set; }
+         public DbSet<Booking> Bookings { get; set; }
+         public DbSet<Agent> Agents { get; set; }
+         public DbSet<Season> Seasons { get; set; }
+        
     }
 }
