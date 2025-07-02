5fac7181f3de84145a5ea74155c69fe0ff3a05f0
MohamedAboElAla <mohamed20300005@gmail.com>
2025-07-02 13:33:13 +0300

    Add Models

---
+++ HotelBooking.Domain/Models/Room.cs
@@ -0,0 +1,41 @@
+﻿using HotelBooking.Application.CustomAttribute;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Domain.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int RoomNumber { get; set; }
+        
+        [Required, StringLength(30)]
+        public string? RoomType { get; set; }
+      
+        [Required, Range(500.00, 10000.00)]
+        public decimal PricePerNight { get; set; }
+        [Required]
+        public bool IsAvailable { get; set; }
+       
+        [Required, StringLength(700, MinimumLength = 20, ErrorMessage = "Description must be between 20 and 700 characters.")]
+        public string? Description { get; set; }
+
+        [AllowedExtensions(new string[] { ".jpg", ".jpeg", ".png" })]
+        public string? ImageUrl { get; set; }
+
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+        
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+       
+    }
+}
---
+++ HotelBooking.Domain/Models/Season.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Domain.Models
+{
+    public class Season
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required, Range(50, 200, ErrorMessage = "Price factor must be between 50 and 200.0.")]
+        public decimal PriceFactor { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+
+    }
+}
