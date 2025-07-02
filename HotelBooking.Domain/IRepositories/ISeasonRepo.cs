019e9938c96b3e70dc5709333eae16fe622856aa
ibrahem2212 <ibrahemmahmoud191272@gmail.com>
2025-07-02 20:47:55 +0200

    add season repo

---
+++ HotelBooking.Domain/IRepositories/ISeasonRepo.cs
@@ -0,0 +1,14 @@
+﻿using HotelBooking.Domain.Models;
+
+namespace HotelBooking.Domain.IRepositories
+{
+    public interface ISeasonRepo
+    {
+        Task<IEnumerable<Season>> GetAllAsync();
+        Task<Season?> GetByIdAsync(int id);
+        Task AddAsync(Season season);
+        void Update(Season season);
+        void Delete(Season season);
+        Task SaveChangesAsync();
+    }
+}
