2db6cc3283ba5a76b07be787c903d32b307b1cce
Mohamed Abo El Ala <89506214+MoHamedAboElAla@users.noreply.github.com>
2025-07-03 13:26:12 +0300

    edit HotelRepo

--- HotelBooking.Infrastructure/Repositories/HotelRepo.cs
+++ HotelBooking.Infrastructure/Repositories/HotelRepo.cs
@@ -10,16 +10,48 @@ using System.Threading.Tasks;
 
 namespace HotelBooking.Infrastructure.Repositories
 {
-    public class HotelRepo : Repo<Hotel>, IHotelRepo
+    public class HotelRepo : IHotelRepo
     {
         private readonly AppDbContext _context;
-        public HotelRepo(AppDbContext context) :base(context)
+        public HotelRepo(AppDbContext context) 
         {
             _context = context;
         }
+        public async Task AddAsync(Hotel entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public void Delete(Hotel entity)
+        {
+            _context.Hotels.Remove(entity);
+        }
+
+        public async  Task<IEnumerable<Hotel>> GetAllAsync()
+        {
+            return   await _context.Hotels.ToListAsync();
+        }
+
+        public async Task<Hotel?> GetByIdAsync(int id)
+        {
+            return await _context.Hotels.FindAsync(id);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(Hotel entity)
+        {
+            _context.Update(entity);
+        }
+
         public async Task<IEnumerable<Hotel>> GetTopRatedHotelsAsync()
         {
             return await _context.Hotels.Where(h => h.Stars >= 4).ToListAsync();
         }
+
+      
     }
 }
