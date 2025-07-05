813b5f2f815d833f9e915f9c7872bc872ba7dda1
MohamedAboElAla <mohamed20300005@gmail.com>
2025-07-05 18:43:42 +0300

    IBookingRepo added

---
+++ HotelBooking.Domain/IRepositories/IBookingRepo.cs
@@ -0,0 +1,23 @@
+﻿using HotelBooking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Domain.IRepositories
+{
+    public interface IBookingRepo
+    {
+        Task<List<Booking>> GetAllAsync();
+        Task<Booking?> GetByIdAsync(int id);
+        Task AddAsync(Booking booking);
+        void Update(Booking booking);
+        void Delete(Booking booking);
+        Task<bool> ExistsAsync(int id);
+        Task<List<Booking>> GetBookingsByHotelIdAsync(int hotelId);
+        Task<List<Booking>> GetBookingsByRoomIdAsync(int roomId);
+        Task SaveChangesAsync();
+
+    }
+}
